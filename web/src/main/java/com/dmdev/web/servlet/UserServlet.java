a50f46dc92c5dd53a91aaad00dfb614ef7a58ddc
Vladislav Timofeev <timofeev.vladislav.sergeevich@gmail.com>
2024-05-06 14:43:32 +0200

    Initial commit

---
+++ web/src/main/java/com/dmdev/web/servlet/UserServlet.java
@@ -0,0 +1,24 @@
+package com.dmdev.web.servlet;
+
+import com.dmdev.service.dto.UserDto;
+import com.dmdev.service.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+
+    private final UserService userService = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Optional<UserDto> user = userService.getUser(1L);
+    }
+}
